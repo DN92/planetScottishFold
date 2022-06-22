f94224a39564f61d25fde0f9581c5557573db9f2
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2022-06-22 14:24:47 -0400

    got webpack working

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'icon-maker'
+      }
+
+    ]
+  }
+}
