c39960f69f6d0900db26987020f1b80ebf59055e
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2022-09-15 03:20:23 -0400

    api bug fix

--- server/api/applications.js
+++ server/api/applications.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Application = require('../db/models').Application
+const { Application } = require('../db').models
 
 router.get('/applications', async (req, res, next) => {
   try {
