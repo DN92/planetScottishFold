bc7df325b6040e9c0e9fffdcb943d10e0afecbc7
DN92 <anatoly.tsinker12@gmail.com>
2022-12-18 18:44:08 -0500

    some jank code

--- server/api/supabase.js
+++ server/api/supabase.js
@@ -5,4 +5,28 @@ router.get('/', includeSupabase, async (req,res,next) => {
   res.send({msg: 'successful endpoint', db: req.supabase})
 })
 
+router.get('/test1', includeSupabase, async(req, res, next ) => {
+  try{
+    if (!req.supabse) {
+      throw Error('missing supabase credentials')
+    }
+
+    const { supabase } = req.supabase
+    const { data, error } = await supabase.storage.from('testkitty1').list('folder', {
+      limit: 100,
+      offset: 0,
+      sortby: {
+        column: 'name',
+        order: 'asc',
+      }
+    })
+    if (error) {
+      throw Error(error)
+    }
+    res.send({data: data})
+  } catch(err) {
+    next(err)
+  }
+})
+
 module.exports = router
--- server/expressMiddleware/includeSupabase.js
+++ server/expressMiddleware/includeSupabase.js
@@ -1,12 +1,18 @@
 const { createClient } = require('@supabase/supabase-js')
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const baseKey = process.env.SUPABASE_KEY ?? ''
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY ?? ''
 
+console.log( 'url:: ', supabaseUrl)
+console.log( 'key:: ', baseKey)
 
 const includeSupabase = (req, res, next) => {
   try{
+    console.log('auth', req.headers.Authorization)
+    const supabase = createClient(supabaseUrl, supabaseKey, {
+      global: {headers: {Authorization: ''}}
+    })
     req.supabase = supabase
     next()
   } catch(err) {
