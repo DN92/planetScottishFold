4973c48ff1c8b79b1747eb99d0466d77fc4e4de6
DN92 <anatoly.tsinker12@gmail.com>
2022-12-18 21:12:07 -0500

    working api for supabase buckets

--- server/api/supabase.js
+++ server/api/supabase.js
@@ -5,25 +5,62 @@ router.get('/', includeSupabase, async (req,res,next) => {
   res.send({msg: 'successful endpoint', db: req.supabase})
 })
 
-router.get('/test1', includeSupabase, async(req, res, next ) => {
+router.get('/byname', includeSupabase, async(req, res, next ) => {
   try{
-    if (!req.supabse) {
-      throw Error('missing supabase credentials')
+    if (!req.supabase) {
+      throw Error('MYERROR:: missing supabase credentials')
     }
-
-    const { supabase } = req.supabase
-    const { data, error } = await supabase.storage.from('testkitty1').list('folder', {
-      limit: 100,
-      offset: 0,
-      sortby: {
-        column: 'name',
-        order: 'asc',
-      }
-    })
+    const { dir } = req.query
+    const supabase = req.supabase
+    const { data, error } = await supabase.storage
+      .from(`${dir}`)
+      .list('', {
+        limit: 100,
+        offset: 0,
+        sortBy: {
+          column: 'name',
+          order: 'desc',
+        }
+      })
+    // const { data, error } = await supabase.storage.listBuckets()
     if (error) {
       throw Error(error)
     }
     res.send({data: data})
+  } catch(err) {
+    next(err)
+  }
+})
+
+router.get('/urlsByBucket', includeSupabase, async(req, res, next ) => {
+  try {
+    if (!req.supabase) {
+      throw Error('LOCAL:: missing supabase credentials')
+    }
+    const supabase = req.supabase
+    const { bucket } = req.query
+    const { data, error } = await supabase.storage
+      .from(`${ bucket }`)
+      .list('', {
+        limit: 100,
+        offset: 0,
+        sortBy: {
+          column: 'name',
+          order: 'desc',
+        }
+      })
+    if (error) {
+      throw Error(error)
+    }
+    const allURLs = await Promise.all(data.map(ele => (
+      supabase.storage
+        .from(`${ bucket }`)
+        .getPublicUrl(`${ ele.name ?? '' }`)
+    )))
+    const final = allURLs.map(ele => (
+      ele.data?.publicUrl
+    ))
+    res.send(final)
   } catch(err) {
     next(err)
   }
--- server/expressMiddleware/includeSupabase.js
+++ server/expressMiddleware/includeSupabase.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js')
 
-const supabaseUrl = process.env.SUPABASE_URL
+const supabaseUrl = process.env.SUPABASE_URL ?? ''
 const baseKey = process.env.SUPABASE_KEY ?? ''
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY ?? ''
 
@@ -9,14 +9,11 @@ console.log( 'key:: ', baseKey)
 
 const includeSupabase = (req, res, next) => {
   try{
-    console.log('auth', req.headers.Authorization)
-    const supabase = createClient(supabaseUrl, supabaseKey, {
-      global: {headers: {Authorization: ''}}
-    })
+    const supabase = createClient(supabaseUrl, supabaseKey)
     req.supabase = supabase
     next()
   } catch(err) {
-    res.send({msg: 'error in supabase middleware', err: err})
+    res.send({msg: 'LOCAL: error in supabase middleware', err: err})
   }
 }
 
