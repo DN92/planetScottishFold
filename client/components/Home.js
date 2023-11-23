b82eb968bbe10f902c3f2bc8eb23cb097209ca46
DN92 <anatoly.tsinker12@gmail.com>
2023-11-22 19:51:06 -0500

    media files nov22

--- client/components/Home.js
+++ client/components/Home.js
@@ -10,7 +10,7 @@ const showMessageBox = true
 const homeComponent = () => {
 
   const messageArray1 = [
-    'DISCOUNTED DELIVERY TO LAGUARDIA AIRPORT, NYC ON 12/09/23.'
+    'DISCOUNTED DELIVERY TO LAGUARDIA/NEWARK AIRPORT, NYC ON 12/09/23.'
   ]
 
   const messageArray2 = [
