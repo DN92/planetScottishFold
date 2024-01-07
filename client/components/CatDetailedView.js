00796b900c1720bc532a45aefb5878547df03327
DN92 <anatoly.tsinker12@gmail.com>
2024-01-07 13:08:23 -0500

    LTP

--- client/components/CatDetailedView.js
+++ client/components/CatDetailedView.js
@@ -57,7 +57,7 @@ const CatDetailedView = () => {
               placeHolderImagePath = '/otherPictures/photoComingSoon.png'
             />
           </>
-          {
+          {/* {
           kittenImages.length > 0 &&
           <>
             <p>{`${cat.name ?? 'UNKNOWN'}'s Past Kittens`}</p>
@@ -66,7 +66,7 @@ const CatDetailedView = () => {
               placeHolderImagePath='/otherPictures/photoComingSoon.png'
             />
           </>
-          }
+          } */}
           <div className='detailedView-text-wrapper'>
             <div className='detailedView-text'>
               <p>{cat.name}</p> <br />
