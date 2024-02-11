1c15e64fb57d663143f8ce9101b4300c129d153a
alex <anatoly.tsinker13@gmail.com>
2024-02-11 13:14:52 -0500

    crc 2-8-24

--- client/components/AvailableKittens.js
+++ client/components/AvailableKittens.js
@@ -19,14 +19,19 @@ const AvailableKittens = () => {
   const messageArray1 = [
     'New kittens will be announced soon!',
     <>
-      <Link Link to='/catDetailedView/mother/12'> Nova </Link> + <Link to='/catDetailedView/father/2'> Cupid </Link> = Born on 2/25/24 &#128512;
+      <Link to='/catDetailedView/mother/11'> Vasilisa </Link> + <Link to='/catDetailedView/father/2'>Cupid </Link> = Born on 1/23/24 &#128512;
     </>,
     <>
-      <Link Link to='/catDetailedView/mother/7'> Ladybug </Link> + <Link to='/catDetailedView/father/2'> Cupid </Link> = Born on 3/01/24 &#128512;
+      <Link  to='/catDetailedView/mother/12'> Nova </Link> + <Link to='/catDetailedView/father/2'> Cupid </Link> = Expected Birthday: 2/25/24
     </>,
+    <>
+      <Link Link to='/catDetailedView/mother/7'> Ladybug </Link> + <Link to='/catDetailedView/father/2'> Cupid </Link> = Expected Birthday: 3/01/24
+    </>,
+    <>
       <Link to='/waitingListForm'>
-      Apply here to recieve SMS notifications.
-      </Link>,
+        Apply here for SMS notifications
+      </Link>
+    </>,
   ]
 
   const getWeight = (obj, filterer) => {
