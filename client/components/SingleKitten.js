b633e65be3e8a6b6c231c06be047eb6463ed7694
DN92 <anatoly.tsinker12@gmail.com>
2024-06-21 22:24:06 -0400

    banner and prices

--- client/components/SingleKitten.js
+++ client/components/SingleKitten.js
@@ -42,7 +42,7 @@ const SingleKitten = (props) => {
               <p>My color is </p>
               <p>{furColor}</p>
               <p>My eyes will be {eyeColor}.</p>
-              <p className='bold-green font-1-rem'>${kitten.price}</p>
+              {/* <p className='bold-green font-1-rem'>${kitten.price}</p> */}
               {/* <LineThroughPTagOverlay
                 message={`Price: $${adjustedPrice}`}
                 classes={['red', ]}
@@ -57,7 +57,7 @@ const SingleKitten = (props) => {
             <>
               <p>{name}</p>
               <p>{kitten.status}</p>
-              {/* <p className='font-1-rem'>${kitten.price}</p> */}
+              <p className='font-1-rem'>${kitten.price}</p>
               {/* <p>Price: ${(Math.round(((parseInt(kitten.price, 10) / 50) * globalPriceModifier ))) * 50}</p> */}
             </>
           }
