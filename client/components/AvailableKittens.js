a9af97392876cbc936fa972611bcbeb4f4107019
DN92 <anatoly.tsinker12@gmail.com>
2024-08-19 03:42:45 -0400

    message boxes set to hidden

--- client/components/AvailableKittens.js
+++ client/components/AvailableKittens.js
@@ -164,7 +164,7 @@ const AvailableKittens = () => {
 
   return (
     <div className='kittens'>
-      <MessageBox
+      {/* <MessageBox
         messageArray={messageArray2}
         options={{
           closeOnCLick: true,
@@ -175,7 +175,7 @@ const AvailableKittens = () => {
         options={{
           closeOnCLick: true,
         }}
-      />
+      /> */}
       <h2 style={{margin: 'auto'}}>Available Kittens</h2>
       {error && <ErrorFill msg={error} />}
       {!error &&
